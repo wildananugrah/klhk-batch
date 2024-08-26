aa0fa6d740708f32fda74c81966a93254f86a75c
BNI <bni@Maverick-900175.lan>
2024-08-26 17:48:59 +0700

    update source update error query

--- src/logics/converter.ts
+++ src/logics/converter.ts
@@ -14,25 +14,31 @@ export class Converter{
         const startTime = process.hrtime();
         console.log(`${formattedDateWithMilliseconds} started.`);
         let imageId = "";
-        try {    
-            const dbResult = await this.odkdb.list(limitImages);
+
+        const dbResult = await this.odkdb.list(limitImages);
             console.log(`${formattedDateWithMilliseconds} number of data: ${dbResult.length}`);
             dbResult.map(async row => { 
-                console.log(`${formattedDateWithMilliseconds} ${row.name} is being converted.`);
-                await this.imageConverter.convert(row.name, row.content, targetFolder);
-                imageId = row.id;
-                await this.odkdb.update(row.id);
-                console.log(`${formattedDateWithMilliseconds} ${row.name} has been converted.`);
+                try {
+                    console.log(`${formattedDateWithMilliseconds} ${row.name} is being converted.`);
+                    await this.imageConverter.convert(row.name, row.content, targetFolder);
+                    await this.odkdb.update(row.id);
+                    console.log(`${formattedDateWithMilliseconds} ${row.name} has been converted.`);
+                } catch (error) {
+                    console.log(error);
+                    await this.odkdb.updateError(row.id);
+                } 
             });
-        } catch (error) {
-            console.log(`imageId: ${imageId}`);
-            await this.odkdb.updateError(imageId);
-            console.log(error);
-        } finally {
-            const [seconds, nanoseconds] = process.hrtime(startTime);
-            const responseTime = seconds * 1000 + nanoseconds / 1e6; // Convert to milliseconds
-            console.log(`processed by ${responseTime.toFixed(2)} ms`);
-        }
+
+        // try {    
+            
+        // } catch (error) {
+        //     await this.odkdb.updateError(imageId);
+        //     console.log(error);
+        // } finally {
+        //     const [seconds, nanoseconds] = process.hrtime(startTime);
+        //     const responseTime = seconds * 1000 + nanoseconds / 1e6; // Convert to milliseconds
+        //     console.log(`processed by ${responseTime.toFixed(2)} ms`);
+        // }
         
     }
 }
